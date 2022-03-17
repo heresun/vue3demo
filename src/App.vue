3ea20833bf5ffa2ccf4be285f246d7c47b15a180
david <1440290539@qq.com>
2022-03-17 21:23:27 +0800

    ceshi

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<!--  vue2中在template里必须有一个根标签，vue3没有这个限制-->
+<!--  vue2中在template里必须有一个根标签，vue3没有这个限制  -->
   <img alt="Vue logo" src="./assets/logo.png">
   <hello-world></hello-world>
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
